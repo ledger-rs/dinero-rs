34bd752cd5cc67c0a216a51577141deb6a3480c9
Claudio Noguera <claudio.noguera@gmail.com>
2021-01-04 14:01:25 +0100

    refactor

--- src/lib/ledger/mod.rs
+++ src/lib/ledger/mod.rs
@@ -5,8 +5,8 @@ mod money;
 mod currency;
 mod transaction;
 
-/// A ledger has (journal) entries. Each of those entries has postings
-/// ledger > entry > posting
+/// A lib.ledger has (journal) entries. Each of those entries has postings
+/// lib.ledger > entry > posting
 /// Each of those postings has an amount of money (a commodity) paired with an account
 /// Each entry has to be balanced
 /// Commodities can change price over time
