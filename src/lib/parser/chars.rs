3a106ea147736b4a6a7e8e6e4d217a31ccbcb8ed
Claudio Noguera <claudio.noguera@gmail.com>
2021-01-04 23:42:16 +0100

    got rid of dangerous warning

--- src/lib/parser/chars.rs
+++ src/lib/parser/chars.rs
@@ -1,4 +1,3 @@
-use crate::Error;
 use super::Tokenizer;
 
 pub(super) enum LineType {
@@ -9,20 +8,18 @@ pub(super) enum LineType {
 pub(super) fn consume_whitespaces(tokenizer: &mut Tokenizer) -> LineType {
     let chars = tokenizer.content.chars().collect::<Vec<char>>();
     while let Some(c) = chars.get(tokenizer.position) {
-        if c.is_whitespace() | (*c == '\r') {
-            tokenizer.position += 1;
-            tokenizer.line_position += 1;
-            continue;
-        } else if *c == '\n' {
+        if *c == '\n' {
             tokenizer.line_index += 1;
             tokenizer.line_position = 0;
             tokenizer.position += 1;
-            return LineType::Blank;
+        } else if c.is_whitespace() | (*c == '\r') {
+            tokenizer.position += 1;
+            tokenizer.line_position += 1;
         } else {
-            match tokenizer.line_position{
+            match tokenizer.line_position {
                 0 => return LineType::Blank,
                 _ => return LineType::Indented,
-            } 
+            }
         }
     }
     LineType::Blank
@@ -30,7 +27,7 @@ pub(super) fn consume_whitespaces(tokenizer: &mut Tokenizer) -> LineType {
 
 pub(super) fn consume_line(tokenizer: &mut Tokenizer) -> String {
     let chars = tokenizer.content.chars().collect::<Vec<char>>();
-    let mut retval:Vec<char> = Vec::new();
+    let mut retval: Vec<char> = Vec::new();
     while let Some(c) = chars.get(tokenizer.position) {
         tokenizer.position += 1;
         if *c == '\n' {
