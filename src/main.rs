8cf109fa1d79aa7b1049a0ba408094630615d9c2
Claudio Noguera <claudio.noguera@gmail.com>
2021-01-19 23:34:36 +0100

    more elegant arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,156 +1,61 @@
+use structopt::StructOpt;
+use dinero::commands::{check, balance, accounts, commodities};
+use std::path::PathBuf;
+use dinero::Error;
+
+#[derive(Debug, StructOpt)]
 enum Command {
-    Print,
-    Balance,
-    Register,
-    Accounts,
-    Codes,
-    Payees,
-    Prices,
-    Commodities,
+    // Print,
+    /// Balance report
+    #[structopt(alias = "bal")]
+    Balance(CommonOpts),
+    // Register,
+    /// List the accounts
+    Accounts(CommonOpts),
+    // Codes,
+    // Payees,
+    // Prices,
+    /// List commodities
+    #[structopt(alias = "currencies")]
+    Commodities(CommonOpts),
+    Check {
+        #[structopt(name = "FILE", short = "f", long = "file", parse(from_os_str))]
+        input: PathBuf,
+    },
 }
 
-use clap::{Arg, App, SubCommand};
-use dinero::commands::{check, balance, accounts, commodities};
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Command line accounting tool",
+version = env ! ("CARGO_PKG_VERSION"),
+author = env ! ("CARGO_PKG_AUTHORS"),
+name = "dinero"
+)]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Command,
+
+}
+
+#[derive(Debug, StructOpt)]
+struct CommonOpts {
+    /// Input file
+    #[structopt(name = "FILE", short = "f", long = "file", parse(from_os_str))]
+    input: PathBuf,
+
+    /// The pattern to look for
+    pattern: Option<String>,
+}
 
 fn main() {
-    let file_arg =Arg::with_name("file")
-        .short("f")
-        .long("file")
-        .value_name("FILE")
-        .help("Ledger file to read")
-        .takes_value(true)
-        .required(false);
-    let matches = App::new("dinero")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about("the command line accounting tool")
-        .subcommand(SubCommand::with_name("check")
-            .about("checks the ledger file"))
-        .subcommand(SubCommand::with_name("balance")
-            .about("prints a balance report")
-            .alias("bal")
-            .arg(file_arg.clone()))
-        .subcommand(SubCommand::with_name("accounts")
-            .about("prints the accounts"))
-        .subcommand(SubCommand::with_name("commodities")
-            .about("prints the accounts")
-            .alias("currencies"))
-        .arg(file_arg)
-        .get_matches();
+    let opt: Opt = Opt::from_args();
+    // println!("{:?}", opt);
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let file = matches.value_of("file").unwrap();
-
-    if let Err(e) = match matches.subcommand_name() {
-        Some("check") => check::execute(file),
-        Some("accounts") => accounts::execute(file),
-        Some("commodities") => commodities::execute(file),
-        Some("balance") => balance::execute(file),
-        None => todo!("No subcommand was used"),
-        _ => unreachable!(), // Assuming you've listed all direct children above, this is unreachable
+    if let Err(e) = match opt.cmd {
+        Command::Balance(options) => { balance::execute(options.input) }
+        Command::Accounts(options) => { accounts::execute(options.input) }
+        Command::Commodities(options) => { commodities::execute(options.input) }
+        Command::Check {input} => {check::execute(input)}
     } {
         eprintln!("{}", e);
     }
 }
-/*
-use std::env;
-use std::path::Path;
-
-mod lib;
-
-#[derive(Debug)]
-enum Command {
-    Print,
-    Balance,
-    Register,
-    Accounts,
-    Codes,
-    Payees,
-    Prices,
-    Commodities,
-}
-
-#[derive(PartialEq)]
-enum Argument {
-    Flat,
-    Tree,
-    Raw,
-    Explicit,
-}
-
-fn main() {
-    if let Err(e) = start() {
-        eprintln!("{}", e);
-    }
-}
-
-fn start() -> Result<(), String> {
-    let mut files: Vec<String> = Vec::new();
-    let mut command = None;
-    let mut arguments = Vec::new();
-    let mut items = Vec::new();
-
-    parse_arguments(&mut files, &mut command, &mut arguments)?;
-
-    match command {
-        None => Err(String::from("Error : No command selected")),
-        Some(command) => {
-            if files.is_empty() {
-                return Err(String::from(
-                    "Error : No file(s) selected. Try --file <file> to select a file",
-                ));
-            }
-            let paths = files.iter().map(|f| Path::new(f)).collect();
-            parsers::parse_files(paths)?;
-
-            // TODO this will not work
-            execute_command(command, arguments, items)
-        }
-    }
-}
-
-fn parse_arguments(
-    files: &mut Vec<String>,
-    command: &mut Option<Command>,
-    arguments: &mut Vec<Argument>,
-) -> Result<(), String> {
-    let mut it = env::args().skip(1);
-
-    while let Some(arg) = it.next() {
-        match arg.as_str() {
-            "--file" | "-f" => match it.next() {
-                None => return Err(String::from("Error : No argument provided for --file")),
-                Some(file_path) => files.push(file_path),
-            },
-            "print" => *command = Some(Command::Print),
-            "accounts" => *command = Some(Command::Accounts),
-            _ => {}
-        }
-    }
-
-    Ok(())
-}
-
-fn execute_command(
-    command: Command,
-    arguments: Vec<Argument>,
-    items: Vec<model::Item>,
-) -> Result<(), String> {
-    match command {
-        Command::Print => {
-            if arguments.contains(&Argument::Explicit) {
-                return commands::print::print_explicit(items);
-            }
-            if arguments.contains(&Argument::Raw) {
-                return commands::print::print_raw(items);
-            }
-            return commands::print::print_raw(items);
-        }
-        Command::Codes => commands::codes::print(items)?,
-        _ => return Err(
-            format!("Command {:?} not implemented", command)
-        )
-    }
-    Ok(())
-}
-*/
