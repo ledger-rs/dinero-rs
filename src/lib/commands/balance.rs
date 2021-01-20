f46b47a68327d5255769145c032fc4ca6e847e02
Claudio Noguera <claudio.noguera@gmail.com>
2021-01-20 20:42:49 +0100

    balances show as tree

--- src/lib/commands/balance.rs
+++ src/lib/commands/balance.rs
@@ -4,9 +4,11 @@ use std::path::PathBuf;
 use colored::Colorize;
 use crate::ledger;
 use crate::ledger::{LedgerElements, Account, Balance, HasName};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::collections::hash_map::RandomState;
 
-pub fn execute(path: PathBuf) -> Result<(), Error> {
+/// Balance report
+pub fn execute(path: PathBuf, flat: bool, show_total: bool) -> Result<(), Error> {
     let mut tokenizer: Tokenizer = Tokenizer::from(&path);
     let mut items = tokenizer.parse()?;
     let mut ledgerelements = ledger::build_ledger(&items)?;
@@ -22,27 +24,80 @@ pub fn execute(path: PathBuf) -> Result<(), Error> {
         }
     }
 
-    let mut total_balance = Balance::new();
-    let mut vec_balances: Vec<(&Account, Balance)> = balances.iter()
-        .map(|x| (*x.0, x.1.clone()))
-        .collect();
-    vec_balances.sort_by(|a, b| a.0.get_name().cmp(b.0.get_name()));
+    // For printing this out, take into account whether it is a flat report or not
+    // if it is not, the parent balances have to be updated
+    let mut vec_balances: Vec<(&str, Balance)>;
+    let mut accounts = HashSet::new();
+    let mut new_balances = HashMap::new();
+    let mut vec: Vec<String>;
+    if !flat {
+        // vec_balances = complete_balance(&balances);
+        for (acc, bal) in balances.iter() {
+            let mut pattern = "".to_string();
+            for part in acc.get_name().split(":") {
+                if pattern.len() > 0 {
+                    pattern.push_str(":");
+                }
+                pattern.push_str(part);
+                accounts.insert(pattern.clone());
+            }
+            new_balances.insert(acc.get_name(), bal.clone());
+        }
+
+        // Sort by depth
+        vec = accounts.iter().map(|x| x.clone()).collect();
+        vec.sort_by(|a, b| a.matches(":").count().cmp(&b.matches(":").count()));
+
+        for account in vec.iter() {
+            let balance = new_balances.iter()
+                .filter(|x| x.0.starts_with(account.as_str()))
+                .fold(Balance::new(), |acc, new| acc + new.1.clone());
+            new_balances.insert(account.as_str().clone(), balance);
+        }
+        vec_balances = new_balances.iter()
+            .map(|x| (x.0.clone(), x.1.clone()))
+            .collect()
+    } else {
+        vec_balances = balances.iter()
+            .map(|x| (x.0.get_name(), x.1.clone()))
+            .collect();
+    }
+
+    vec_balances.sort_by(|a, b| a.0.cmp(b.0));
     for (account, bal) in vec_balances.iter() {
+        let mut first = true;
         for (_, money) in bal.balance.iter() {
+            if !first {println!();}
+            first = false;
             print!("{:>20}", format!("{}", money));
         }
-        total_balance = total_balance + bal.to_owned();
-        println!("  {}", account.get_name().blue());
-    }
-    print!("{}", "--------------------");
-
-    if total_balance.is_zero() {
-        print!("\n{:>20}", "0");
-    } else {
-        for (_, money) in total_balance.balance.iter() {
-            print!("\n{:>20}", format!("{}", money));
+        if flat {println!("  {}", account.blue());} else {
+            let n = account.split(":").count();
+            let text = account.split(":").last().unwrap();
+            for _ in 0..n {print!("  ");}
+            println!("{}", text.blue());
         }
     }
-    println!("");
+
+    // Print the total
+    if show_total {
+        // Calculate it
+        let mut total_balance = balances.iter()
+            .fold(Balance::new(), |acc, x| acc + x.1.to_owned());
+        print!("{}", "--------------------");
+
+        if total_balance.is_zero() {
+            print!("\n{:>20}", "0");
+        } else {
+            for (_, money) in total_balance.balance.iter() {
+                print!("\n{:>20}", format!("{}", money));
+            }
+        }
+        println!();
+    }
+
+    // We're done :)
     Ok(())
 }
+
+
