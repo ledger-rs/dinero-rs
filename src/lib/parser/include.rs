2f633424022e22d73ba5f2aeb11c94270266d5bc
Claudio Noguera <claudio.noguera@gmail.com>
2021-01-21 01:18:46 +0100

    Include was not working properly

--- src/lib/parser/include.rs
+++ src/lib/parser/include.rs
@@ -7,8 +7,16 @@ use std::path::PathBuf;
 /// Handles include directive
 pub(super) fn parse<'a>(tokenizer: &'a mut Tokenizer) -> Result<Vec<Item>, Error> {
     chars::consume_str(tokenizer, &"include ".to_string())?;
-    let pattern = chars::get_line(tokenizer);
+    let mut pattern = String::new();
     let mut files: Vec<PathBuf> = Vec::new();
+    if let Some(current_path) = tokenizer.file {
+        // let mut parent = format!("{:?}", current_path.parent().unwrap()).replace('"', "");
+        let mut parent = current_path.parent().unwrap().to_str().unwrap().to_string();
+        if parent.len() == 0 { parent.push('.') }
+        parent.push('/');
+        pattern.push_str(parent.as_str());
+    }
+    pattern.push_str(chars::get_line(tokenizer).as_str());
     for entry in glob(&pattern).expect("Failed to read glob pattern") {
         match entry {
             Ok(path) => {
