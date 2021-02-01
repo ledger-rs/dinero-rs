813239a90a3da983a163b2e254748a693ae614fb
Claudio Noguera <claudio.noguera@gmail.com>
2021-02-01 03:04:01 +0100

    a test for bad config files #3

--- tests/test_commands.rs
+++ tests/test_commands.rs
@@ -7,10 +7,26 @@ fn test_balance() {
         "tests/demo.ledger",
         "--init-file",
         "tests/example_ledgerrc",
+        "--real",
     ]
     .iter()
     .map(|x| x.to_string())
     .collect();
     let res = dinero::run_app(args);
     assert!(res.is_ok());
+}
+
+#[test]
+#[should_panic(expected = "Bad config file \"tests/example_bad_ledgerrc\"\nThis line should be a comment but isn\'t, it is bad on purpose.")]
+fn test_bad_ledgerrc() {
+    let args: Vec<String> = vec![
+        "executable",
+        "bal",
+        "--init-file",
+        "tests/example_bad_ledgerrc"
+    ]
+    .iter()
+    .map(|x| x.to_string())
+    .collect();
+    let res = dinero::run_app(args);
 }
