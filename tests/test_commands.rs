76f2ddbb1f9a9cebcde3383b6ea6af91917e890d
Claudio Noguera <claudio.noguera@gmail.com>
2021-02-01 02:44:58 +0100

    Improve coverage of main.rs by using a sample ledgerrc file #3

--- tests/test_commands.rs
+++ tests/test_commands.rs
@@ -1,9 +1,16 @@
 #[test]
 fn test_balance() {
-    let args: Vec<String> = vec!["executable", "bal", "-f", "tests/demo.ledger"]
-        .iter()
-        .map(|x| x.to_string())
-        .collect();
+    let args: Vec<String> = vec![
+        "executable",
+        "bal",
+        "-f",
+        "tests/demo.ledger",
+        "--init-file",
+        "tests/example_ledgerrc",
+    ]
+    .iter()
+    .map(|x| x.to_string())
+    .collect();
     let res = dinero::run_app(args);
     assert!(res.is_ok());
 }
