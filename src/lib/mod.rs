2dbabf37d6f794b4c04362d4e0fd6d6e8acb2101
Claudio Noguera <claudio.noguera@gmail.com>
2021-02-01 01:05:12 +0100

    refactor main for better testability #3

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -2,8 +2,10 @@ pub mod commands;
 mod error;
 mod filter;
 mod list;
+mod main;
 pub mod models;
 pub mod parser;
 
 pub(crate) use error::{Error, LedgerError, ParserError};
 pub use list::List;
+pub use main::main;
