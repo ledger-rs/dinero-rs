a63d22871e3b3d707dc1f7cac66f48333afeba88
Claudio Noguera <claudio.noguera@gmail.com>
2021-02-06 15:29:50 +0100

    [ci skip] change comments

--- src/lib/models/price.rs
+++ src/lib/models/price.rs
@@ -34,27 +34,9 @@ pub enum PriceType {
     PerUnit,
 }
 
-// function Dijkstra(Graph, source):
-//        dist[source]  := 0                     // Distance from source to source is set to 0
-//        for each vertex v in Graph:            // Initializations
-//            if v ≠ source
-//                dist[v]  := infinity           // Unknown distance function from source to each node set to infinity
-//            add v to Q                         // All nodes initially in Q
-//
-//       while Q is not empty:                  // The main loop
-//           v := vertex in Q with min dist[v]  // In the first run-through, this vertex is the source node
-//           remove v from Q
-//
-//           for each neighbor u of v:           // where neighbor u has not yet been removed from Q.
-//               alt := dist[v] + length(v, u)
-//               if alt < dist[u]:               // A shorter path to u has been found
-//                   dist[u]  := alt             // Update distance of u
-//
-//       return dist[]
-//   end function
 /// Convert from one currency to every other currency
 ///
-/// This uses an implementation of the Dijkstra algorithm to find the shortest path from every
+/// This uses an implementation of the [Dijkstra algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm) to find the shortest path from every
 /// currency to the desired one
 pub fn conversion(
     currency: Rc<Currency>,
