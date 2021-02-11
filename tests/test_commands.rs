f2c2534c5806f4f1dd347a866f4709974cbfcb10
Claudio Noguera <claudio.noguera@gmail.com>
2021-02-12 00:18:31 +0100

    aim for 100% coverage in filter.rs

--- tests/test_commands.rs
+++ tests/test_commands.rs
@@ -93,3 +93,15 @@ fn tag_filter() {
 
     test_args(args);
 }
+
+
+#[test]
+/// Check that the tag filter works
+fn account_filter() {
+    let args = &["bal", "-f", "examples/demo.ledger", "--flat", "travel"];
+    let assert_1 = Command::cargo_bin("dinero").unwrap().args(args).assert();
+    let output = String::from_utf8(assert_1.get_output().to_owned().stdout).unwrap();
+    assert_eq!(output.lines().into_iter().count(), 1);
+
+    test_args(args);
+}
