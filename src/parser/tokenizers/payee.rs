1d640bd011674f946ed71f636f65571f9b3d0122
Claudio Noguera <claudio.noguera@gmail.com>
2021-02-22 21:41:25 +0100

    some tests for payees #3

--- src/parser/tokenizers/payee.rs
+++ src/parser/tokenizers/payee.rs
@@ -77,3 +77,26 @@ pub(crate) fn parse(tokenizer: &mut Tokenizer) -> Result<Payee, ParserError> {
         origin: Origin::FromDirective,
     })
 }
+
+mod tests {
+    use super::*;
+    use crate::models::HasName;
+    #[test]
+    fn parse_ko() {
+        let input = "payee ACME  ; From the Looney Tunes\n\tWrong Acme, Inc.\n".to_string();
+        let mut tokenizer = Tokenizer::from(input);
+        let payee_raw = parse(&mut tokenizer);
+        assert!(payee_raw.is_err());
+    }
+
+
+   #[test]
+    fn parse_ok() {
+        let input = "payee ACME\n\talias Acme, Inc.\n".to_string();
+        let mut tokenizer = Tokenizer::from(input);
+        let payee_raw = parse(& mut tokenizer);
+        assert!(payee_raw.is_ok());
+        let payee = payee_raw.unwrap();
+        assert_eq!(payee.get_name(), "ACME");
+    }
+}
