3988a0e60949415248821ad75e9709115af5d61e
Claudio Noguera <claudio.noguera@gmail.com>
2021-08-16 23:38:58 +0200

    fix test #116

--- tests/test_repl.rs
+++ tests/test_repl.rs
@@ -3,11 +3,10 @@ use rexpect::spawn;
 // const CARGO: &'static str = env!("CARGO");
 
 #[test]
-fn test_repl() {
+fn repl_open_and_close() {
     let command = "cargo run -- --init-file tests/example_files/empty_ledgerrc -f tests/example_files/demo.ledger";
     let mut p = spawn(command, Some(10000)).unwrap();
     p.exp_regex(">> ").unwrap();
-    p.send_line("anonymous").unwrap();
     p.send_line("exit").unwrap();
     p.exp_eof().unwrap();
     assert!(true);
