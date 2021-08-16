d14dc7f02d2c31a3a1d1584c9fe5ca9b5a75d43f
Claudio Noguera <claudio.noguera@gmail.com>
2021-08-16 23:48:30 +0200

    longer timeout in repl test #116

--- tests/test_repl.rs
+++ tests/test_repl.rs
@@ -5,7 +5,7 @@ use rexpect::spawn;
 #[test]
 fn repl_open_and_close() {
     let command = "cargo run -- --init-file tests/example_files/empty_ledgerrc -f tests/example_files/demo.ledger";
-    let mut p = spawn(command, Some(10000)).unwrap();
+    let mut p = spawn(command, Some(120_000)).unwrap();
     p.exp_regex(">> ").unwrap();
     p.send_line("exit").unwrap();
     p.exp_eof().unwrap();
