fe777cca4c0ff35b87a2cc08ae13af2b7e378149
Claudio Noguera <claudio.noguera@gmail.com>
2021-08-17 00:46:38 +0200

    hope it works in github actions #116

--- tests/test_repl.rs
+++ tests/test_repl.rs
@@ -1,15 +1,22 @@
+use assert_cmd::{prelude::OutputOkExt, Command};
+use common::{test_args, test_err};
 use rexpect::spawn;
-
+mod common;
 // const CARGO: &'static str = env!("CARGO");
 
+
 #[test]
-fn repl_open_and_close() {
-    let command =
-        "dinero --init-file tests/example_files/empty_ledgerrc -f tests/example_files/demo.ledger";
-    // let command = format!("dinero {}", options);
-    let mut p = spawn(command, Some(10_000)).unwrap();
-    p.exp_regex(">> ").unwrap();
-    p.send_line("exit").unwrap();
-    p.exp_eof().unwrap();
-    assert!(true);
+fn stdin_repl() {
+    let args1 = &[
+        "--init-file",
+        "tests/example_files/empty_ledgerrc",
+        "-f",
+        "tests/example_files/demo.ledger",
+    ];
+    let command = Command::cargo_bin("dinero")
+        .unwrap()
+        .args(args1)
+        .write_stdin("exit")
+        .ok();
+    assert!(command.is_ok());
 }
