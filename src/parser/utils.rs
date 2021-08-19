f4e85fec65ae697f1510ede01673b16c21649fd5
Claudio Noguera <claudio.noguera@gmail.com>
2021-08-19 16:35:23 +0200

    rational to float function, still wip #115

--- src/parser/utils.rs
+++ src/parser/utils.rs
@@ -2,7 +2,7 @@
 
 use super::{GrammarParser, Rule};
 use chrono::NaiveDate;
-use num::{BigInt, BigRational};
+use num::{BigInt, BigRational, Signed};
 use pest::iterators::Pair;
 
 use pest::Parser;
@@ -71,4 +71,40 @@ pub(crate) fn parse_string(string: Pair<Rule>) -> String {
         }
         _ => string.as_str().trim().to_string(),
     }
+}
+
+pub(crate) fn rational2float(number: &BigRational, decimals: usize) -> f64 {
+    let base: i32 = 10;
+    let mut integer_part = number.trunc();
+    let decimal_part = (number.fract() * BigInt::from(base.pow(decimals as u32 + 2)))
+        .abs()
+        .trunc();
+
+    let mut decimal_str = if decimals == 0 {
+        String::new()
+    } else {
+        format!("{:0width$}", decimal_part.numer(), width = decimals + 2)
+    };
+    if decimals > 0 {
+        decimal_str.truncate(decimals + 1);
+
+        let mut decimal = if u64::from_str(&decimal_str).unwrap() % 10 >= 5 {
+            u64::from_str(&decimal_str).unwrap() / 10 + 1
+        } else {
+            u64::from_str(&decimal_str).unwrap() / 10
+        };
+        let len = format!("{}", decimal).len();
+        if len == decimals + 1 {
+            decimal = 0;
+            if integer_part.is_positive() {
+                integer_part += BigInt::from(1);
+            } else {
+                integer_part -= BigInt::from(1);
+            }
+        }
+        let decimal_separator = ".";
+        decimal_str = format!("{}{:0width$}", decimal_separator, decimal, width = decimals);
+    }
+    let repr = format!("{}{}", integer_part, decimal_str);
+    f64::from_str(repr.as_str()).unwrap()
 }
