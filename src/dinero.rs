3c332153bda784e3d1aa285da752316197629371
Claudio Noguera <claudio.noguera@gmail.com>
2021-08-30 23:11:09 +0200

    explicitly exit with code 0

--- src/dinero.rs
+++ src/dinero.rs
@@ -2,7 +2,7 @@ use std::env;
 
 fn main() {
     match dinero::run_app(env::args().collect()) {
-        Ok(_) => {}
+        Ok(_) => std::process::exit(0),
         Err(x) => {
             let message = format!("{}", x);
             if !message.is_empty() {
