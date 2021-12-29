c9231fc4fc7008c118fb09b448ee2f926a0af971
Claudio Noguera <claudio.noguera@gmail.com>
2021-12-29 14:10:58 +0100

    handle conversion error in roi command

--- src/dinero.rs
+++ src/dinero.rs
@@ -1,5 +1,8 @@
 use std::env;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dinero::run_app(env::args().collect())
+fn main() {
+    match dinero::run_app(env::args().collect()) {
+        Ok(_) => std::process::exit(0),
+        Err(_) => std::process::exit(1),
+    }
 }
